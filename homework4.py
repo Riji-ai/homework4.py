f5fe949b007c66eeabcfb7fad00897c8e651b42e
Riji-ai <riji.dav@yandex.ru>
2024-07-21 19:25:42 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,12 @@
+name = input ('my-string: ')
+enigma = 2000
+muzon = int(input('Напиши число до 2000 '))
+lol = enigma-muzon
+print('Слыш? Чё это ты тут написал?', name)
+print('А вот и остаточек!',lol,'возьмите, распишитесь')
+print('my-string'.upper())
+print('my-string'.lower())
+print('my-string'.replace('-','' ))
+print('my-string'.replace('m','M'))
+print('my-string'.replace('g','G'))
+
